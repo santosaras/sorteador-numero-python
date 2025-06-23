c6715625bd8c40218feca108b3829ed62123b99c
Sara Santos <saramcssantos@gmail.com>
2025-06-22 23:51:45 -0300

    Altera menu

--- sorteia_numeros.py
+++ sorteia_numeros.py
@@ -2,23 +2,22 @@ import random
 
 print("-- Sorteador de números --")
 
-opcao = int(input("Digite 1 para sortear ou 2 para sair: "))
+while True:
+    opcao = int(input("Digite:\n1 - Sortear\n2 - Sair: "))
+    match opcao:
+        case 1:
+            quantidade = int(input("Digite a quantidade de números a serem sorteado: "))
+            minimo = int(input("Digite o valor mínimo a ser sorteado: "))
+            maximo = int(input("Digite o valor máximo a ser sorteado: "))
 
-while opcao == 1:
-        quantidade = int(input("Digite a quantidade de números a serem sorteado: "))
-        minimo = int(input("Digite o valor mínimo a ser sorteado: "))
-        maximo = int(input("Digite o valor máximo a ser sorteado: "))
+            resultado = []
 
-        resultado = []
-
-        for i in range(quantidade):
-            sorteados = random.randint(minimo, maximo)
-            resultado.append(sorteados)
-
-        print(sorted(set(resultado)))
-
-        opcao = int(input("Digite 1 para sortear ou 2 para sair: "))
-
-        if opcao == 2:
+            for i in range(quantidade):
+                sorteados = random.randint(minimo, maximo)
+                resultado.append(sorteados)
+            print(sorted(set(resultado)))
+        case 2:
+            print("Encerrando sorteador de número")
             break
-print("Encerrando sorteador de número")
+        case _:
+            print("Opção inválida!")
